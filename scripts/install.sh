641f44bb7ac2084b2332bd4d6206c0b9542e43d6
dalyIsaac <isaac.daly@outlook.com>
2021-07-25 17:31:18 +1200

    Updated Bourne install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,18 +3,19 @@
 # Downloads and installs mstodo from GitHub to the given directory, or /usr/local/bin (by default)
 DIRECTORY=${1:-/usr/local/bin}
 
-cd $DIRECTORY
-
+# Download from URL
 EXE_URL=https://github.com/dalyIsaac/mstodo/releases/download/$VERSION/mstodo-$VERSION-linux-amd64.tar.gz
-curl -L $EXE_URL | tar -xz
+curl --silent --location --output mstodo.tar.gz $EXE_URL
+sudo tar -xzf mstodo.tar.gz -C /usr/local/bin
+rm mstodo.tar.gz
 
 # Create .mstodo/config.yaml in the user's home directory
 cd ~
-mkdir .mstodo
+mkdir -p .mstodo
 cd .mstodo
 
 CONFIG_URL=https://github.com/dalyIsaac/mstodo/releases/download/$VERSION/config.yaml
-curl -L $CONFIG_URL > config.yaml
+curl -s -L $CONFIG_URL > config.yaml
 
 # Tell the user they need to populate config.yaml
 echo "Please populate config.yaml with your configuration."
