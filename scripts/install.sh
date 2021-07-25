18a29189268565d582a4caeed3fd07ebcb9590ee
dalyIsaac <isaac.daly@outlook.com>
2021-07-25 15:15:11 +1200

    Removed VERSION variable from install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,7 +2,6 @@
 
 # Downloads and installs mstodo from GitHub to the given directory, or /usr/local/bin (by default)
 DIRECTORY=${1:-/usr/local/bin}
-VERSION=VERSION
 curl -L https://github.com/dalyIsaac/mstodo/releases/download/v1.0.0/mstodo-v1.0.0-linux-amd64.tar.gz | tar -xz
 
 # Create .mstodo/config.yaml in the user's home directory
